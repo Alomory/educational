af4babe1820af92f21323eb2eafef68a1cd96668
garylkz <gary.github@gmail.com>
2022-11-30 00:47:20 +0800

    UwU

---
+++ src/main/java/com/garylkz/c3013/lab3/Task3.java
@@ -0,0 +1,27 @@
+package com.garylkz.c3013.lab3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean skillIssue = true;
+        String next = "";
+        while (skillIssue) {
+            try {
+                next = s.nextLine();
+                Integer.parseInt(next, 2);
+                skillIssue = false;
+            } catch (NumberFormatException e) {
+                System.out.println("lol");
+            }
+        }
+        Task2 intStack = new Task2();
+        Arrays.stream(next.split("")).forEach(i -> intStack.push(Integer.parseInt(i)));
+        int sum = 0, power = 0;
+        while (!intStack.isEmpty()) sum += (intStack.pop() * (Math.pow(2, power++)));
+        System.out.println(sum);
+        s.close();
+    }
+}
