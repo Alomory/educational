c06fcd9f2a43ef4612c44cacfe31ed3606bc1512
garylkz <gary.github@gmail.com>
2022-12-09 20:44:28 +0800

    Added lab 4 tasks

---
+++ src/main/java/com/garylkz/c3013/lab4/Task1.java
@@ -0,0 +1,68 @@
+package com.garylkz.c3013.lab4;
+
+public class Task1 {
+    char q[]; 
+    int front, rear, size; 
+    
+    public Task1(int m) {
+        size = m; 
+        q = new char[size]; 
+        front = rear = -1;
+    }
+
+    char dequeue() { 
+        if (isEmpty()) {
+            System.out.println("Queue is empty"); 
+            return (char) 250; 
+        } else { 
+            char x = q[front]; 
+            front++; 
+            return x;
+        } 
+    } 
+
+    void enqueue(char n) {
+        if (isFull())  System.out.println("Queue is full");
+        else {
+            if (front == -1) front = rear = 0; 
+            else rear++;
+            q[rear] = n;
+        }
+    }
+    
+    char peek() { 
+        return q[front]; 
+    } 
+
+    boolean isEmpty() { 
+        return (front == -1 || front > rear); 
+    }
+
+    boolean isFull() { 
+        return (rear == size - 1); 
+    } 
+
+    public String toString() {
+        String s = ""; 
+        for (int i = 0; i < q.length; i++) s += i+1+"="+q[i]+" ";
+        return s;
+    }
+
+    public static void main(String[] args) {
+        // Task1 myQueue = new Task1(10);
+        Task1 myQueue = new Task1(11);
+        myQueue.enqueue('a'); 
+        myQueue.enqueue('b'); 
+        myQueue.enqueue('c'); 
+        myQueue.enqueue('d'); 
+        myQueue.enqueue('e'); 
+        myQueue.enqueue('f'); 
+        myQueue.enqueue('g'); 
+        myQueue.enqueue('h'); 
+        myQueue.enqueue('i'); 
+        myQueue.enqueue('j'); 
+        System.out.println("First element in queue: "+myQueue.peek()); 
+        myQueue.enqueue('k'); 
+        System.out.println(myQueue);
+    }
+} 
---
+++ src/main/java/com/garylkz/c3013/lab4/Task3.java
@@ -0,0 +1,112 @@
+package com.garylkz.c3013.lab4;
+
+import java.util.Arrays;
+
+public class Task3<E> {
+    private int currentSize; // Current Circular Queue Size
+    private E[] circularQueueElements;
+    private int maxSize; // Circular Queue maximum size
+    private int rear;// rear position of Circular queue(new element enqueued at rear).
+    private int front; // front position of Circular queue(element will be dequed from front).
+
+    public Task3(int maxSize) {
+        this.maxSize = maxSize;
+        circularQueueElements = (E[]) new Object[this.maxSize];
+        currentSize = 0;
+        front = -1;
+        rear = -1;
+    }
+
+    public void enqueue(E item) throws QueueFullException {
+        if (isFull())
+            throw new QueueFullException("Circular Queue is full. Element cannot be added");
+        else {
+            rear = (rear + 1) % circularQueueElements.length;
+            circularQueueElements[rear] = item;
+            currentSize++;
+            if (front == -1)
+                front = rear;
+        }
+    }
+
+    public E dequeue() throws QueueEmptyException {
+        E deQueuedElement;
+        if (isEmpty())
+            throw new QueueEmptyException("Circular Queue is empty. Element cannot be retrieved");
+        else {
+            deQueuedElement = circularQueueElements[front];
+            circularQueueElements[front] = null;
+            front = (front + 1) % circularQueueElements.length;
+            currentSize--;
+        }
+        return deQueuedElement;
+    }
+
+    public boolean isFull() {
+        return (currentSize == circularQueueElements.length);
+    }
+
+    public boolean isEmpty() {
+        return (currentSize == 0);
+    }
+
+    public String toString() {
+        return Arrays.toString(circularQueueElements);
+    }
+
+    public static void main(String[] args) {
+        Task3<Integer> c = new Task3<>(8);
+        c.enqueue(15);
+        c.enqueue(16);
+        c.enqueue(17);
+        c.enqueue(18);
+        c.enqueue(19);
+        c.enqueue(20);
+        c.enqueue(21);
+        c.enqueue(22);
+        System.out.println(c);
+        System.out.println("Dequeue");
+        System.out.println(c.dequeue());
+        c.enqueue(23);
+        System.out.println("Enqueue");
+        System.out.println(c);
+
+        Task3<String> d = new Task3<>(8);
+        d.enqueue("A");
+        d.enqueue("B");
+        d.enqueue("C");
+        d.enqueue("D");
+        d.enqueue("E");
+        d.enqueue("F");
+        d.enqueue("G");
+        d.enqueue("H");
+        System.out.println(d);
+        System.out.println("Dequeue");
+        System.out.println(d.dequeue());
+        d.enqueue("I");
+        System.out.println("Enqueue");
+        System.out.println(d);
+    }
+}
+
+class QueueFullException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public QueueFullException() {
+        super();
+    }
+
+    public QueueFullException(String message) {
+        super(message);
+    }
+}
+
+class QueueEmptyException extends RuntimeException {
+    public QueueEmptyException() {
+        super();
+    }
+
+    public QueueEmptyException(String message) {
+        super(message);
+    }
+}
