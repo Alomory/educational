7b3cc65a6d92778fa93fb6d69d912f575bb8d7c2
garylkz <37288203+garylkz@users.noreply.github.com>
2023-01-07 16:00:20 +0000

    Updated activity1 activity2

--- c3123/lab6/activity2.sql
+++ c3123/lab6/activity2.sql
@@ -1,4 +1,4 @@
-USE activity1;
+USE Lab6;
 
 \! echo "find the number of airports that originated from Malaysia"
 
@@ -19,3 +19,29 @@ WHERE b.bookingstatus = 'Confirmed' AND a.airlinename = 'AirAsia' AND b.departur
 SELECT MAX(f.ticketprice) AS MaxPrice, MIN(f.ticketprice) AS MinPrice
 FROM flightdetails f, booking b, airport a
 WHERE (b.bookingdate BETWEEN '2016-04-01' AND '2016-09-15') AND a.country = 'Malaysia';
+
+\! echo "To retrieve the information you've requested, you can use the following queries: Retrieve airport code, airport name, number of flight and average flight duration for all airport order by airport name in ascending order."
+
+SELECT airportcode, airportname, COUNT(flightno) AS num_flights, AVG(duration) AS avg_duration
+FROM airport
+JOIN flight ON airport.airportcode = flight.airportcode
+JOIN flightdetails ON flight.flightno = flightdetails.flightno
+GROUP BY airport.airportcode
+ORDER BY airportname ASC;
+
+\! echo "Display profile id, profile name, his state and the frequent passenger flies for passenger who make more than one booking."
+
+SELECT profile.prfid, prfname, prfstate, COUNT(booking.prfid) AS num_bookings
+FROM profile
+JOIN booking ON profile.prfid = booking.prfid
+GROUP BY profile.prfid
+HAVING COUNT(booking.prfid) > 1;
+
+\! echo "Display passenger id, name, total amount ticket price booked by every passenger where the total amount of ticket price is exceeded the average ticket price offered by Air Asia airlines."
+
+SELECT profile.prfid, prfname, SUM(flightdetails.ticketprice) AS total_ticket_price
+FROM profile
+JOIN booking ON profile.prfid = booking.prfid
+JOIN flightdetails ON booking.flightno = flightdetails.flightno
+GROUP BY profile.prfid
+HAVING SUM(flightdetails.ticketprice) > (SELECT AVG(ticketprice) FROM flightdetails);
