894efd0b455e390d706c0cc28002d8e1edeb62bc
garylkz <37288203+garylkz@users.noreply.github.com>
2023-01-14 09:28:25 +0000

    Added project

---
+++ c3123/project/unf.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS Project;
+CREATE DATABASE Project;
+USE Project;
+
+CREATE TABLE OrderSystem (
+    order_id INTEGER PRIMARY KEY,
+    quantity INTEGER,
+    order_total DECIMAL,
+    order_status VARCHAR(20),
+    customer_name VARCHAR(50),
+    customer_email VARCHAR(100),
+    customer_phone VARCHAR(20),
+    product_name VARCHAR(100),
+    product_price DECIMAL,
+    product_stock INTEGER,
+    vendor_name VARCHAR(100),
+    vendor_contact_name VARCHAR(50),
+    vendor_email VARCHAR(100),
+    vendor_phone VARCHAR(20),
+    note_content VARCHAR(500)
+);
